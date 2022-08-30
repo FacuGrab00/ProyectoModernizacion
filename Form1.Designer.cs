7663d10b96afe7060fec48e7663a564977fd3778
Facundo Martin Grab <facugrab00@gmail.com>
2022-08-30 09:24:47 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace ProyectoModernizacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.dgvExcel = new System.Windows.Forms.DataGridView();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvExcel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(674, 468);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(153, 29);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Abrir Archivo";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // dgvExcel
+            // 
+            this.dgvExcel.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvExcel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvExcel.Location = new System.Drawing.Point(50, 39);
+            this.dgvExcel.Name = "dgvExcel";
+            this.dgvExcel.RowHeadersWidth = 51;
+            this.dgvExcel.RowTemplate.Height = 29;
+            this.dgvExcel.Size = new System.Drawing.Size(650, 316);
+            this.dgvExcel.TabIndex = 1;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(515, 468);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(153, 29);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar Archivo";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 509);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dgvExcel);
+            this.Controls.Add(this.btnAbrir);
+            this.Name = "Form1";
+            this.Text = "Sistema de Control de Personal";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvExcel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.DataGridView dgvExcel;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoModernizacion
+{
+    public static class StringExtensions
+    {
+        public static string UpperFirstChar(this string input)
+        {
+            string[] name = input.Split(" ");
+            if (string.IsNullOrEmpty(input))
+            {
+                return null;
+            }
+            
+            return char.ToUpper(name[0][0]) + name[0].Substring(1) + " " + char.ToUpper(name[1][0]) + name[1].Substring(1);
+        }
+    }
+}
