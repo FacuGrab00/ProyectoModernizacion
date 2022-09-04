c08142bf518e67637b8adca97120ec30904292de
Facundo Martin Grab <facugrab00@gmail.com>
2022-09-03 21:53:21 -0300

    caso doble salida y caso doble entrada funcionando

--- FormRegistros.cs
+++ FormRegistros.cs
@@ -50,6 +50,24 @@ namespace ProyectoModernizacion
                     regActual.Horas = TimeSpan.Zero;
                     registrosProcesados.Add(regActual);
                 }
+
+                if(CasoDobleEntrada(regActual, regAnterior))
+                {
+                    regActual = manejadorExcel.Registros[i+1];
+                    if (CasoComun(regActual, regAnterior))
+                    {
+                        hora = regActual.Horario - regAnterior.Horario;
+                        regActual.Horas = hora;
+                        registrosProcesados.Add(regActual);
+                        i++;
+                    }
+                    else
+                    {
+                        regActual.Observacion = "No se marcó la Salida";
+                        regActual.Horas = TimeSpan.Zero;
+                        registrosProcesados.Add(regActual);
+                    }
+                }
             }
         }
         
@@ -78,10 +96,21 @@ namespace ProyectoModernizacion
             return bandera;
         }
 
-        //private bool CasoDobleEntrada(Registro regActual, Registro regAnterior)
-        //{
-        //    return true;
-        //}
+        private bool CasoDobleEntrada(Registro regActual, Registro regAnterior)
+        {
+            bool bandera = false;
+            if (regActual.ID.Equals(regAnterior.ID))
+            {
+                if (regActual.Horario.Day.Equals(regAnterior.Horario.Day))
+                {
+                    if (regActual.Estado == "Registro de entrada" && regAnterior.Estado == "Registro de entrada")
+                    {
+                        bandera = true;
+                    }
+                }
+            }
+            return bandera;
+        }
 
         private bool CasoDobleSalida(Registro regActual, Registro regAnterior)
         {
@@ -96,7 +125,6 @@ namespace ProyectoModernizacion
                     }
                 }
             }
-
             return bandera;
         }
 
